78e848e9d50361c301d284dcd2a0b2967431ec64
GalateeM <galatee.m@gmail.com>
2025-12-19 11:18:44 +0100

    Ajout de commentaires

--- backend/src/main/kotlin/com/n7/api/kcat/repository/LitterMeasurementRepository.kt
+++ backend/src/main/kotlin/com/n7/api/kcat/repository/LitterMeasurementRepository.kt
@@ -3,5 +3,8 @@ package com.n7.api.kcat.repository
 import com.n7.api.kcat.model.LitterMeasurement
 import org.springframework.data.repository.CrudRepository
 
+/*
+Repository des donnees "LitterMeasurement" faisant le lien avec la BD postgresql
+ */
 interface LitterMeasurementRepository : CrudRepository<LitterMeasurement, Int> {
 }
