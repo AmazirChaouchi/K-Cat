0f5cee0341a2f3c5215c28f60498106da816d14c
LeilaMartinet <100203225+LeilaMartinet@users.noreply.github.com>
2025-12-19 11:39:43 +0100

    profile

--- app/app/src/main/java/com/example/k_cat/MainActivity.kt
+++ app/app/src/main/java/com/example/k_cat/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.k_cat
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -9,6 +12,9 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContentView(R.layout.activity_main)
 
         val navHostFragment =
@@ -17,5 +23,11 @@ class MainActivity : AppCompatActivity() {
 
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottom_nav)
         bottomNav.setupWithNavController(navController)
+
+        ViewCompat.setOnApplyWindowInsetsListener(bottomNav) { view, insets ->
+            val navBars = insets.getInsets(WindowInsetsCompat.Type.navigationBars())
+            view.setPadding(0, 0, 0, navBars.bottom)
+            insets
+        }
     }
 }
