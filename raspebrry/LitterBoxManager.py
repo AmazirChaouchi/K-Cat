2ef664e83c629623b03a39c2588a2f207399489b
Dodize <gastelluelodie@gmail.com>
2026-01-15 18:54:33 +0100

    maj avec passage fonctionnel

--- raspebrry/LitterBoxManager.py
+++ raspebrry/LitterBoxManager.py
@@ -2,7 +2,7 @@
 
 # TEMP: LED
 from gpiozero import LED
-from time import time, localtime
+from time import time, localtime, sleep, strftime
 import requests
 
 from DoorSensor import DoorSensor
@@ -20,16 +20,16 @@ class LitterBoxManager:
         # TODO: Initialize Pin Layout from file
         self.doorSensor = DoorSensor(DOOR_SENSOR_PIN)
         self.weightSensor = WeightSensor(WEIGHT_DOUT_PIN, WEIGHT_SCK_PIN)
-
-        # TEMP: LED
         self.led = LED(LED_PIN)        
 
 
     def setup(self):
+        print("setup")
+        self.led.on()
         # TODO: Pairing
-        # TODO: Calibration
         self.weightSensor.setScale()
-        print("setup()")
+        sleep(1)
+        self.led.off()
 
 
     def run(self):
@@ -40,51 +40,57 @@ class LitterBoxManager:
         # measuredWeight[0]: litter box is empty
         # measuredWeight[1]: there is a cat in the litter box
         # measuredWeight[1] - measuredWeight[0] = cat's weight
-        measuredWeight = [-1.0, -1.0]
+        measuredWeight = [None, None]
         # measureTime[0] = start time i.e. t0
         # measureTime[1] = stop time i.e. t1
-        measuredOpenTime = [-1.0, -1.0]
-        measuredCloseTime = [-1.0, -1.0]
+        measuredOpenTime = [None, None]
+        measuredCloseTime = [None, None]
+        must_be_cleaned = False
 
         while(True):
             currentDoorState = self.doorSensor.getStatus()
             if (currentDoorState == "open" and previousDoorState == "closed"):
-                # TODO: timeCloseStart = timeCloseStop = None
+                # print("closed -> open")
                 measuredWeight[0] = self.weightSensor.getWeight()
-                # TODO: Start 2" openTimer
                 measuredOpenTime[0] = time()
-                # TODO: Take care of the duration
-                measuredCloseTime[0] = time()
-                previousDoorState = currentDoorState
             elif (currentDoorState == "closed" and previousDoorState == "open"):
-                # TODO: Save timeOpenStop i.e. t1
+                # print("open -> closed")
                 measuredOpenTime[1] = time()
+                measuredCloseTime[0] = time()
+                measuredWeight[1] = self.weightSensor.getWeight()
                 # If timeOpenStop - timeOpenStart < 2" then reset timers
                 if (measuredOpenTime[1] - measuredOpenTime[0] < 2.0):
                     measuredOpenTime = [None, None]
-                    measuredCloseTime[0] = time()
-                    measuredWeight[1] = self.weightSensor.getWeight()
             elif (currentDoorState == "closed" and previousDoorState == "closed"):
+                # print("closed -> closed")
                 measuredCloseTime[1] = time()
-                # TODO: If measuredCloseTime < 2", do nothing
-                # TODO: Else, send information reagarding weight to the server
-                measuredWeight[1] = self.weightSensor.getWeight()
-                if (measuredCloseTime[1] - measuredCloseTime[0] > 2.0):
+                # If measuredCloseTime < 2", do nothing
+                # Else, send information reagarding weight to the server
+                if (measuredWeight[0] != None and measuredWeight[1] != None and (measuredWeight[1] > measuredWeight[0] + 20) and measuredCloseTime[1] - measuredCloseTime[0] > 2.0):
+                # if (measuredCloseTime[1] - measuredCloseTime[0] > 2.0):
                     # Send a weight information to the server using the API
                     payload = {
-                        "litiereId": "12345",
+                        "litiereId": "1318",
                         "poids": measuredWeight[1] - measuredWeight[0],
-                        "timestamp": localtime()
+                        "timestamp": strftime("%Y-%m-%dT%H:%M:%S", localtime())
                     }
                     response = requests.post("https://k-cat.onrender.com/api/litter-measurements", json=payload)
-                    print(response.status_code) 
+                    print(response.json())
                     must_be_cleaned = response.json()   # True ou False
                     print("Doit être nettoyée ?", must_be_cleaned)
+                    # RAZ weight to not seend infinitly
+                    measuredWeight[0] = measuredWeight[1] = None
             else: # currentDoorState == "open" and previousDoorState = "open"
+                # print("open -> open")
                 pass
 
             if (must_be_cleaned):
-                led.on()
+                self.led.on()
+
+            previousDoorState = currentDoorState
+            if (measuredWeight[0] != None and measuredWeight[1] != None):
+                print(f"[DEBUG]: weight = {measuredWeight[1] - measuredWeight[0]}")
+            sleep(0.5)
 
                 
 if __name__ == "__main__":
