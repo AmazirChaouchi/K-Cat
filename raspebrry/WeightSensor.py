17470d522a903a51f42d0440275f14b8684f43b5
Amazir Chaouchi <amazir.chaouchi@gmail.com>
2026-01-15 14:28:33 +0100

    fix: add missing self in WeightSensor

--- raspebrry/WeightSensor.py
+++ raspebrry/WeightSensor.py
@@ -4,13 +4,13 @@ class WeightSensor:
 
     def __init__(self, doutPinNumber, sckPinNumber):
         self.hx = HX711(doutPinNumber, sckPinNumber)
-        hx.set_reading_format("MSB", "MSB")
+        self.hx.set_reading_format("MSB", "MSB")
         self.referenceUnit = 470.1586
-        hx.set_reference_unit(referenceUnit)
-        hx.reset()
-        hx.tare()
+        self.hx.set_reference_unit(referenceUnit)
+        self.hx.reset()
+        self.hx.tare()
 
 
     def getWeight():
-        return hx.get_weight(5)
+        return self.hx.get_weight(5)
 
